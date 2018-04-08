388d88ff6c78434ce18d95adeb200a17cada3c54
Dawid Kossowski <dawidkossowski@gmail.com>
2018-04-08 19:55:18 +0200

    deleted model

--- src/containers/model/Address.js
+++
@@ -1,8 +0,0 @@
-export class Address {
-    constructor(street, suite, city, zipcode) {
-        this.street = street;
-        this.suite = suite;
-        this.city = city;
-        this.zipcode = zipcode;
-    }
-}
